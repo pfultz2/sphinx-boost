55fd5cfc315691b85e945e19cb7c6e78364f3c85
Paul <pfultz2@yahoo.com>
2016-10-24 00:18:05 -0500

    Add initial boost theme

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+
+setup(
+    name="sphinx-boost",
+    version=VERSION,
+    url='https://github.com/pfultz2/sphinx-boost',
+    license='boost',
+    description='Boost theme for sphinx',
+    author='Paul Fultz II',
+    author_email='pfultz2@yahoo.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
