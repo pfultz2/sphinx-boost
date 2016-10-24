cea388778a89038187949c519e460876495f8540
Paul <pfultz2@yahoo.com>
2016-10-24 09:32:49 -0500

    Adjust table styles

--- sphinx_boost/__init__.py
+++ sphinx_boost/__init__.py
@@ -9,4 +9,4 @@ __version_full__ = __version__
 def get_html_theme_path():
     """Return list of HTML theme paths."""
     cur_dir = os.path.abspath(os.path.dirname(__file__))
-    return [cur_dir]
+    return cur_dir
